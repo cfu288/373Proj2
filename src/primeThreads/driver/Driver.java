ec83e8c7e77a5db2069ed048065a86800b69a6da
Christopher Fu <cfu6@binghamton.edu>
2016-09-25 13:23:11 -0400

    logger and driver updated

--- src/primeThreads/driver/Driver.java
+++ src/primeThreads/driver/Driver.java
@@ -4,16 +4,17 @@ import primeThreads.util.*;
 
 public class Driver{
 	public static void main(String args[]) {
-
-		System.out.println("\n The args[0] is: " + args[0]);
-        Logger l = new Logger();
-        l.setDebugValue(4);
-        l.writeMessage("Testing level 4",4);
-        l.writeMessage("Testing level 3",3);
-        l.writeMessage("Testing level 2",2);
-        l.writeMessage("Testing level 1",1);
-        l.writeMessage("Testing level 0",0);
-
+        if(args.length < 3) System.out.println("INCORRECT ARGS(4)");
+        else{
+		    String inp = args[0]; String out = args[1];
+            int NUM_THREADS = Integer.parseInt(args[2]);
+            int DEBUG_VALUE = Integer.parseInt(args[3]);
+            Logger l = new Logger();
+            l.setDebugValue(DEBUG_VALUE);
+            l.writeOutput("Testing level 4",4);
+            l.writeOutput("Testing level 3",3);
+            l.writeOutput("Testing level 2",2);
+        }
 	} // end main(...)
 
 } // end public class Driver
--- src/primeThreads/util/Logger.java
+++ src/primeThreads/util/Logger.java
@@ -1,30 +1,36 @@
 package primeThreads.util;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+
 public class Logger{
-    //public static enum DebugLevel {CONSTRUCTOR};
-    //private static DebugLevel debugLevel;
     private static int debugLevel;
-    
-    /*public static void setDebugValue (int levelIn) {
-	    switch (levelIn) {
-	        case 4: debugLevel = DebugLevel.CONSTRUCTOR; break;
-	    }
-    }*/
 
     public static void setDebugValue (int levelIn) {
 	    debugLevel = levelIn;
     }
-    // @return None
+    
     public static void writeMessage (String message, int levelIn ) {
 	    if (levelIn == debugLevel)
 	        System.out.println(message);
     }
-
-    public static void writeOutput(){
-        
+    
+    //http://stackoverflow.com/questions/2885173/how-to-create-a-file-and-write-to-a-file-in-java
+    public static void writeOutput(String message, int levelIn){
+        if(levelIn == debugLevel){
+            try(Writer writer = new BufferedWriter(new OutputStreamWriter( new FileOutputStream("log.txt"), "utf-8"))){
+                writer.write(message);
+            }catch(IOException e ){
+                System.out.println("Exception thrown :" + e);
+            }
+        }
     }
 
-    public String toString() {
+    public String toString(){
         return "Debug Level is " + debugLevel;
     }
 }
