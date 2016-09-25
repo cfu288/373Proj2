b6de4163586e9e06ede4daaf35109bab36e55ff7
Mariuxi Yagual <mariuxi1@live.com>
2016-09-25 15:31:39 -0400

    workerthread constructor and run added

--- src/primeThreads/threadMgmt/WorkerThread.java
+++ src/primeThreads/threadMgmt/WorkerThread.java
@@ -5,27 +5,43 @@ import primeThreads.util.*;
 public class WorkerThread implements Runnable  {
 	
 	private FileProcessor fileProcessor;
-	private Student student;
+	private Student student = null;
 	//private ObjectPool;
    /**
     * Constructor
     */
-	public WorkerThread(FileProcessor fp, Student stud //Object pool, 
+	public WorkerThread(FileProcessor fp//Object pool, 
 			){
 		fileProcessor = fp;
-    	student = stud;
-  
+		student = new Student();
     }
 	/**
 	 * 
 	 */
     public void run() {
-    	//Invoke a method in the FileProcessor to read one line as a string
-    	String[] studentInfo = fileProcessor.getStringArray();
-    	//Run your algorithm to assign courses to this student.
-    	//Store the results in the data structure in the Results instance
+   // 	try{
+    		//Invoke a method in the FileProcessor to read one line as a string
+    		String[] studentInfo = fileProcessor.getStringArray();
+    		student.setName(studentInfo[0]);
+    		student.setPreferences(Integer.parseInt(studentInfo[1]), Integer.parseInt(studentInfo[2]),
+    			Integer.parseInt(studentInfo[3]), Integer.parseInt(studentInfo[4]));
     	
+    		student.printPreferences();
+    		//Run your algorithm to assign courses to this student.
+    	
+    	
+    		//Store the results in the data structure in the Results instance
+    		System.out.println(student.toString());
+    //	}
+    //	catch(InterruptedException exception){
+    		
+   // 	}
     }
+   
+    
+    
+    
+    
     /*
 	 * assign courses according to the students preferences.
 	 * @param Vector<courses>
