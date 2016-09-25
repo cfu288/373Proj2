be49e2fc22b8fb3cb1640aac5d1207b5ae146210
Christopher Fu <cfu6@binghamton.edu>
2016-09-25 17:23:08 -0400

    objpool

--- src/primeThreads/objects/ObjectPool.java
+++ src/primeThreads/objects/ObjectPool.java
@@ -1,11 +1,52 @@
 package primeThreads.objects;
 
-public class ObjectPool{
+import java.util.Hashtable;
+import primeThreads.objects.Course;
+
+/*public abstract class ObjectPool{
 	
-    //private array of objects
+    private Hashtable locked, unlocked;
+    public synchronized <T> aquire(String s); 
+    public synchronized void release(String s); 
+    public String toString();
+    
+}*/
+
+public class ObjectPool{
+    //private hashtable of objects
+    //use hash since we look up by key, 0(1) lookup 
+	private Hashtable<String, Course> unlocked = new Hashtable<String, Course>(); // 4 classes for
+	private Hashtable<String, Course> locked = new Hashtable<String, Course>(); // 4 classes for
+    private String[] names = {"A", "B", "C", "D", "E", "F", "G"};
+    
+    public ObjectPool(){
+        for(int i =0;i<4;i++){
+            Course tmp = new Course(names[i]);
+            unlocked.put("names[i]",tmp);
+        }
+    }
+
+    public synchronized Course aquire(String s){//checkout
+        Course c;
+        c = unlocked.get(s);
+        if(c != null){
+            locked.put(s,c);
+            unlocked.remove(s);
+        }
+        return c;
+    }
+    
+    public synchronized void release(String s){
+        Course c;
+        c = locked.get(s);
+        if(c != null){
+            unlocked.put(s,c);
+            locked.remove(s);
+        }
+    }
+    
 
     public String toString() {
 		return "Objpool";
 	}
-
 }
