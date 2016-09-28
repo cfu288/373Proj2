161ece6c1d2c91164b51f024e973ed8f0f1a85d7
Christopher Fu <cfu6@binghamton.edu>
2016-09-27 22:26:36 -0400

    added catch statements

--- src/primeThreads/threadMgmt/WorkerThread.java
+++ src/primeThreads/threadMgmt/WorkerThread.java
@@ -24,7 +24,6 @@ public class WorkerThread implements Runnable  {
 	 * 
 	 */
     public void run() {
-    	//try{
     		//Invoke a method in the FileProcessor to read one line as a string
         log.writeMessage("run() called",3);
         String[] studentInfo = fileProcessor.getStringArray();
@@ -36,26 +35,24 @@ public class WorkerThread implements Runnable  {
                             Integer.parseInt(studentInfo[3]), Integer.parseInt(studentInfo[4]),  
                             Integer.parseInt(studentInfo[5]), Integer.parseInt(studentInfo[6]),
                             Integer.parseInt(studentInfo[7]));
-    	    //student.printPreferences();
     		
             //Run your algorithm to assign courses to this student.
     		ObjectPool op = ObjectPool.getInstance();  
     		for(int initPref = 1; initPref <= NUM_CLASSES && (student.hasAllCourses() == 0); initPref++){
-    			String className = student.findPreference(initPref);
+    	    String className = student.findPreference(initPref);
+    	    try{
     			Course newCourse = op.aquire(className);
-    			if(newCourse != null){
     			if(op.checkAvailability(newCourse) > 0){
     				student.enroll(newCourse);
     				op.addStudent(newCourse);
     				student.increaseTotalPreference(initPref);
     			} 
-                }else System.out.println("NULL" );
     			op.release(className);
+    	    }catch(InterruptedException e){System.out.println("inturrupt: "+e);}
     		}
     		//Store the results in the data structure in the Results instance
             res.saveResults(student); 
             studentInfo = fileProcessor.getStringArray();
     	}	
-    	//}catch(InterruptedException e){System.out.println("inturrupt: " + e);}
     }
     } 
