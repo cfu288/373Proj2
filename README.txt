551a4ea4e11c6dbcfd20299363c0ca08dc0113ee
Christopher Fu <cfu6@binghamton.edu>
2016-09-29 22:01:56 -0400

    added proper subclassing and casting of objectpool from superobjectpool

--- README.txt
+++ README.txt
@@ -12,6 +12,10 @@ ant -buildfile src/build.xml run -Darg0=firstarg
 
 ## To run by specifying args in build.xml (just for debugging, not for submission)
 ant -buildfile src/build.xml run
+ant -buildfile src/build.xml run1 //Run with debug val of 1 
+ant -buildfile src/build.xml run2 //Run with debug val of 2
+ant -buildfile src/build.xml run3
+ant -buildfile src/build.xml run4
 
 ## To create tarball for submission
 ant -buildfile src/build.xml tarzip
--- src/primeThreads/threadMgmt/WorkerThread.java
+++ src/primeThreads/threadMgmt/WorkerThread.java
@@ -2,6 +2,7 @@ package primeThreads.threadMgmt;
 
 import primeThreads.objects.Student;
 import primeThreads.objects.Course;
+import primeThreads.objects.SuperObjectPool;
 import primeThreads.objects.ObjectPool;
 import primeThreads.util.Logger;
 import primeThreads.util.FileProcessor;
@@ -12,13 +13,13 @@ public class WorkerThread implements Runnable  {
 	private FileProcessor fileProcessor;
 	private Student student = null;
 	private Results res = null;
-	private ObjectPool o = null;
+	private SuperObjectPool o = null;
     private Logger log = null;
     private int NUM_CLASSES = 7;
     /**
     * Constructor
     */
-	public WorkerThread(FileProcessor fp, ObjectPool op, Results rs, Logger l){
+	public WorkerThread(FileProcessor fp, SuperObjectPool op, Results rs, Logger l){
 		fileProcessor = fp;
 		res = rs;
         o = op;
@@ -33,7 +34,7 @@ public class WorkerThread implements Runnable  {
         //Invoke a method in the FileProcessor to read one line as a string
         log.writeMessage("run() called",3);
         String[] studentInfo = fileProcessor.getStringArray();
-        while(studentInfo!=null && studentInfo.length > 1 ){
+        while(studentInfo!= null && studentInfo.length > 1 ){
 	        Student student = new Student();
 		log.writeMessage("Student constructed",4);
             student.setName(studentInfo[0]);
@@ -44,18 +45,18 @@ public class WorkerThread implements Runnable  {
                             Integer.parseInt(studentInfo[7]));
     		
             //Algorithm to assign courses to this student.
-    		ObjectPool op = ObjectPool.getInstance();  
+    		SuperObjectPool op = ObjectPool.getInstance();  
     		for(int initPref = 1; initPref <= NUM_CLASSES && (student.hasAllCourses() == 0); initPref++){
     	    String className = student.findPreference(initPref);
     	        try{
-    			    Course newCourse = op.aquire(className);
-    			    if(op.checkAvailability(newCourse) > 0){
+    			    Course newCourse = ((ObjectPool) op).aquire(className);
+    			    if( ((ObjectPool) op).checkAvailability(newCourse) > 0){
     				    student.enroll(newCourse);
-    				    op.addStudent(newCourse);
+    				    ((ObjectPool) op).addStudent(newCourse);
     				    student.increaseTotalPreference(initPref);
     			    } 
-    			    op.release(className);
-    	        }catch(InterruptedException e){System.out.println("inturrupt: "+e);}
+    			    ((ObjectPool) op).release(className);
+    	        }catch(InterruptedException e){System.out.println("inturrupt: "+e);System.exit(0);}
     		}
     		
             //Store the results in the data structure in the Results instance
